8dc77e215cc07a47ed3c6228bdac901864b2d776
Nicholas Williams <nickwill820@gmail.com>
2024-07-28 12:58:03 -0400

    Added new camera system to levels
    - Added 2 bounds cameras in basement and main floor levels
    - Changes bounds variable to be in world space instead of local

--- Assets/Scripts/Camera Control/BoundsCamera.cs
+++ Assets/Scripts/Camera Control/BoundsCamera.cs
@@ -5,25 +5,29 @@ namespace ShadowAlchemy.CameraControl
 {
     public class BoundsCamera : CinemachineVirtualCamera
     {
-        [SerializeField] private Bounds localBounds = new(Vector3.zero, Vector3.one);
+        [SerializeField] private Bounds bounds = new(Vector3.zero, Vector3.one);
         [Header("Debug")]
         [SerializeField] private Color gizmoColor = Color.white;
         [SerializeField] private bool showBounds;
 
-        public Bounds Bounds => new(transform.position + localBounds.center, localBounds.size);
+        [ContextMenu("Move Bounds To Transform")]
+        private void MoveBoundsToTransform()
+        {
+            bounds.center = transform.position;
+        }
 
         public float DistanceTo(Vector3 point) => Vector3.Distance(transform.position, point);
 
         public bool CloserThan(BoundsCamera boundsCamera, Vector3 point) => DistanceTo(point) <= boundsCamera.DistanceTo(point);
 
-        public bool Contains(Vector3 point) => Bounds.Contains(point);
+        public bool Contains(Vector3 point) => bounds.Contains(point);
 
         private void OnDrawGizmosSelected()
         {
             if (!showBounds) return;
 
             Gizmos.color = gizmoColor;
-            Gizmos.DrawWireCube(Bounds.center, Bounds.size);
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
         }
     }
 }
